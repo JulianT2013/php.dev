e1e808deeb368dfe041115f3a5615d54b52a317d
JulianT2013 <jh_tapping@yahoo.com.au>
2018-08-09 15:19:47 +1000

    Updating & deleting data

--- index.php
+++ index.php
@@ -8,13 +8,24 @@ $rows = $database->resultset();
 
 $post = filter_input_array( INPUT_POST, FILTER_SANITIZE_STRING );
 
-if ( $post['submit'] ) {
+if( isset( $_POST['delete'] ) ) {
+	$delete_id = $_POST['delete_id'];
+	$database->query('DELETE FROM posts WHERE id = :id');
+	$database->bind(':id', $delete_id);
+	$database->execute();
+}
+
+if ( isset( $post['submit'] ) ) {
+	$id = ( isset( $post['id'] ) ) ? $post['id'] : '';
 	$title = $post['title'];
 	$body = $post['body'];
-	
-	$database->query( 'INSERT INTO posts ( title, body ) VALUES(:title, :body)' );
-	$database->bind( ':title', $title );
-	$database->bind( ':body', $body );
+
+	$database->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+	$database->bind(':title', $title);
+	$database->bind(':body', $body);
+	$database->bind(':id', $id);
+	$database->execute();
+
 	$database->execute();
 	if( $database->lastInsertId() ) {
 		echo '<p>Post Added.</p>';
@@ -28,6 +39,8 @@ $rows = $database->resultset();
 
 <h1>Add Post</h1>
 <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+	<label>Post ID</label><br />
+	<input type="text" name="id" placeholder="Specify ID" /><br /><br />
 	<label>Post Title</label><br />
 	<input type="text" name="title" placeholder="Add a title" /><br /><br />
 	<label>Post Body</label><br />
@@ -38,11 +51,15 @@ $rows = $database->resultset();
 <h1>Posts</h1>
 <div>
 
-<?php foreach ( $rows as $row ): ?>
-	<div>
-		<h3><?= $row['title'] ?></h3>
-		<p><?= $row['body'] ?></p>
-	</div>
+	<?php foreach ( $rows as $row ): ?>
+		<div>
+			<h3><?= $row['title'] ?></h3>
+			<p><?= $row['body'] ?></p>
+			<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+				<input type="hidden" name="delete_id" value="<?= $row['id'] ?>">
+				<input type="submit" name="delete" value="Delete" />
+			</form>
+		</div>
 
 	<?php endforeach; ?>
 </div>
